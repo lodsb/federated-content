e53a3cf7bc93206746ab9e4899ae7b25ae145a0a
lodsb <lodsb@lodsb.org>
2015-06-25 06:49:55 +0200

    ...

--- README.md
+++ README.md
@@ -1,112 +1,11 @@
-x-mixtaper
-==========
+#federated content
 
-x-mixtaper is a Web Components (Polymer based) audio player with playlist functionality built for sharing/publishing mixtapes and playlists.
+video/audio player + content distribution - runs solely in the browser
 
-It reads what to play from the `mixtapes.json`, which supports several playlists.
+TODO:
 
-* now with video support
-* up'd to polymer 1.0
-* playlist is a property, can be dynamically changed
+persistence: localstorage or remotestorage.js
+source (media) distribution from multiple uris
 
-## Demo
--..
-
-## Install
-Install the component using [Bower](http://bower.io/):
-
-```sh
-$ bower install x-mixtaper --save
-```
-
-## Usage
-1. Import Web Components' polyfill:
-
-    ```html
-    <script src="bower_components/platform/platform.js"></script>
-    ```
-
-2. Import Custom Element:
-
-    ```html
-    <link rel="import" href="bower_components/x-mixtaper/dist/x-mixtaper.html">
-    ```
-
-3. Add your own mixtapes.json and mp3s (or copy the samples provided)
-
-    ```json
-    {
-      "tape1": {
-        "title": "My first mixtape",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "songs": [{
-          "title": "Ukulele Song",
-          "artist": "Rui Ramos",
-          "file":"/mp3/1/RuiRamos-UkuleleSong.mp3",
-          "length": "04:22"
-        },{
-          "title": "I Dreamed a Dream",
-          "artist": "Wing",
-          "file":"/mp3/1/02 I Dreamed a Dream.m4a",
-          "length": "03:21"
-        }]
-      }
-    }
-    ```
-
-
-4. Start using it!
-
-    ```html
-    <x-mixtaper tape="tape1"></x-mixtaper>
-    ```
-
-Remember that for the audio skipping to work you must be using a web server with HTTP 1.1 byte-range requests support.
-
-Feel free to use it and submit pull requests!
-
-## Development
-
-In order to run it locally you'll need to fetch some dependencies and a basic server setup.
-
-* Install [Bower](http://bower.io/) & [Grunt](http://gruntjs.com/):
-
-    ```sh
-    $ [sudo] npm install -g bower grunt-cli
-    ```
-
-* Install local dependencies:
-
-    ```sh
-    $ bower install && npm install
-    ```
-
-* To test your project, start the development server and open `http://localhost:8000`.
-
-    ```sh
-    $ grunt server
-    ```
-
-* To build the distribution files before releasing a new version.
-
-    ```sh
-    $ grunt build
-    ```
-
-* To provide a live demo, send everything to `gh-pages` branch.
-
-    ```sh
-    $ grunt deploy
-    ```
-
-## Contributing
-
-1. Fork it!
-2. Create your feature branch: `git checkout -b my-new-feature`
-3. Commit your changes: `git commit -m 'Add some feature'`
-4. Push to the branch: `git push origin my-new-feature`
-5. Submit a pull request :D
-
-## License
-
-[MIT License](http://opensource.org/licenses/MIT)
+note to myself:
+check webtorrent
---
+++ src/boombox.js
@@ -0,0 +1,187 @@
+
+    //TODO export playlist from media sources
+
+    var db = new loki('main.json');
+
+    var playlists = db.addCollection('playlists');
+    var media = db.addCollection('media');
+    var imports = db.addCollection('imports');
+
+    playlists.ensureUniqueIndex('hash');
+    media.ensureUniqueIndex('hash');
+    imports.ensureUniqueIndex('uri');
+
+    function search(query) {
+      //TODO: make search case insensitive!
+      // wasting cycles
+      var playlistSearch = playlists.find({'title': {'$contains': query}});
+
+      var artistSearch= media.find({'artist': {'$contains': query}});
+      var mediaSearch = media.find({'title': {'$contains': query}});
+
+      var result = {};
+      result.playlists = playlistSearch;
+      result.artists   = artistSearch;
+      result.media     = mediaSearch;
+
+
+      return result;
+    }
+
+
+    /*TODO: check media avail, random avail media url load, score media url?
+      latency, throughput?
+    */
+
+    function stringHash(string){
+    	var hash = 0;
+    	if (string.length == 0) return hash;
+    	for (i = 0; i < string.length; i++) {
+    		char = string.charCodeAt(i);
+    		hash = ((hash<<5)-hash)+char;
+    		hash = hash & hash; // Convert to 32bit integer
+    	}
+    	return hash;
+    }
+
+    function hashJson(json) {
+      var jsonString = JSON.stringify(jsonString);
+
+      console.log(jsonString);
+    }
+
+    function mediaHash(mediaJson) {
+      // ignore url and tags
+
+      var mediaAuthor =  mediaJson.author  ? mediaJson.author : "UNDEF" ;
+      var mediaTitle =   mediaJson.title   ? mediaJson.title  : "UNDEF" ;
+      var mediaType =    mediaJson.type    ? mediaJson.type   : "UNDEF" ;
+      var mediaSubtype = mediaJson.subtype ? mediaJson.subtype: "UNDEF" ;
+
+      var mediaString = mediaAuthor + mediaTitle + mediaType + mediaSubtype;
+
+      return stringHash(mediaString);
+    }
+
+    function uniqueAddToDB(data, dataAsString) {
+      var playlistHash = stringHash(dataAsString);
+
+      data.hash = playlistHash;
+
+      if(!playlists.by('hash', playlistHash)) {
+
+
+        // check in media first
+        var mediaItems = data.media;
+
+        var modifiedMediaItems = [];
+
+        itemsLength = mediaItems.length;
+        for(var mi = 0; mi < itemsLength; mi ++) {
+          var mediaItem = mediaItems[mi];
+
+          var mediaItemHash = mediaHash(mediaItem);
+
+          mediaItem.hash = mediaItemHash;
+
+          var existingMediaItem = media.by('hash', mediaItemHash);
+
+          // if unknown media, then add it
+          if(!existingMediaItem) {
+            var origUri       = mediaItem.uri;
+
+            var newUriEntry   = {};
+            newUriEntry.uri   = origUri;
+            newUriEntry.status= "unchecked";
+
+            delete mediaItem.uri;
+
+            mediaItem.uris = [newUriEntry];
+
+            console.log(mediaItem);
+
+            media.insert(mediaItem);
+
+            modifiedMediaItems.push(mediaItem);
+
+          }
+          // else add entry to uris if it doesn exist
+          else {
+            var uriKnown = false;
+
+            var existringUris       = existingMediaItem.uris;
+            var existringUrisLength = existingMediaItem.uris.length;
+
+            // search through uris for already existing entry
+            for(var ui = 0; ui < existringUrisLength; ui ++) {
+              if(existringUris[ui].uri == mediaItem.uri) {
+                uriKnown = true;
+                break;
+              }
+            }
+
+            if(!uriKnown) {
+              var origUri       = mediaItem.uri;
+
+              var newUriEntry   = {};
+              newUriEntry.uri   = origUri;
+              newUriEntry.status= "unchecked";
+
+              existingMediaItem.uris = existingMediaItem.uris.concat([newUriEntry]);
+
+              modifiedMediaItems.push(existingMediaItem);
+            }
+
+          }
+
+
+        }
+
+        // modify playlist and
+        data.media=modifiedMediaItems;
+        // insert it
+        playlists.insert(data);
+
+      }
+    }
+
+    function importJson(url, func, failedFunc) {
+      // TODO: validate, and throw everything away, we dont know!
+
+      if(!imports.by('uri', url)) {
+
+        jQuery.ajax({
+          url: url,
+        })
+        .done(function(jsonString) {
+          var data = jQuery.parseJSON(jsonString);
+
+          uniqueAddToDB(data, jsonString);
+
+          imports.insert({
+            'title': data.title, // shold also have a root directory indicator + colors for my imports list
+            'uri'  : url,
+            'crawl': false, // should lateron be used to retrigger crawling references
+            'type' : 'playlist' // playlist or bucket
+          });
+
+          func(data);
+
+        })
+        .fail(function() {
+          failedFunc(jQuery.error());
+          console.log("failed ajax json request");
+        })
+
+      } else {
+        failedFunc("Already exists!");
+      }
+
+    }
+
+    jQuery(document).ready(function() {
+      jQuery.ajaxSetup({
+          cache: false
+      });
+
+    });
